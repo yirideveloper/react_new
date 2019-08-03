13c5f646a6afb7fe8f7fce96355825c91caebbf6
yirideveloper <topyiri1113@gmail.com>
2019-08-03 23:33:30 +0530

    Added documentation for available routes

--- models/User.js
+++ models/User.js
@@ -1,40 +1,11 @@
-var mongoose = require('mongoose');
-const isEmail = require('validator/lib/isEmail');
+var mongoose = require('mongoose');  
 var UserSchema = new mongoose.Schema({  
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    required: true,
-    trim: true,
-    unique: true,
-    validate(value) {
-      if (!isEmail(value)) {
-        throw new Error(`Invalid value: email. Expected email address. Received ${ value }.`);
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+  name: String,
+  email: String,
+  password: String,
   secretToken: String,
   isActivated: Boolean,
-}, {
-  timestamps: true,
 });
-
-UserSchema.virtual('items', {
-  ref: 'Item',
-  foreignField: 'owner',
-  localField: '_id',
-});
-
 mongoose.model('User', UserSchema);
 
 module.exports = mongoose.model('User');
